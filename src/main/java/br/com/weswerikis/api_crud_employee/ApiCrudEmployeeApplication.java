a7c39bfa722640f29025c52f60c6e6a0739c6a8d
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-05 09:06:06 -0300

    feat [main-ApiCrudEmployeeApplication] : Add ApiCrudEmployeeApplication main class

---
+++ src/main/java/br/com/weswerikis/api_crud_employee/ApiCrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis.api_crud_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudEmployeeApplication.class, args);
+	}
+
+}
