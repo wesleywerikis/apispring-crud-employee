a3c4d7fd429907ed2aa54fc4d3045a13147b3546
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-05 09:16:44 -0300

    feat [test-ApiCrudEmployeeApplication] : Add ApiCrudEmployeeApplication main class

---
+++ src/test/java/br/com/weswerikis/api_crud_employee/ApiCrudEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis.api_crud_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
