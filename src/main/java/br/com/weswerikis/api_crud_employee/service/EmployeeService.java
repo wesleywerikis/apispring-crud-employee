e73cc9d15df1100ea0cc03325734c9e2acca70c6
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-09 16:59:29 -0300

    feat [service-EmployeeService] : Adds EmployeeService interface with createEmployee method

---
+++ src/main/java/br/com/weswerikis/api_crud_employee/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package br.com.weswerikis.api_crud_employee.service;
+
+import br.com.weswerikis.api_crud_employee.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+}
