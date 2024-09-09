3ad9f6aa2c1d344713e0b26824dd5bf2112ace17
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-09 11:28:07 -0300

    refactor [repository-EmployeeRepository] : Add @Repository annotation to EmployeeRepository

--- src/main/java/br/com/weswerikis/api_crud_employee/repository/EmployeeRepository.java
+++ src/main/java/br/com/weswerikis/api_crud_employee/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package br.com.weswerikis.api_crud_employee.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.weswerikis.api_crud_employee.entity.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
