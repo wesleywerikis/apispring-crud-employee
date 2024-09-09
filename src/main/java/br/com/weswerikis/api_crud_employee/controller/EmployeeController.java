9564c056c6353a496408f2ecee5df1088c383b55
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-09 17:33:12 -0300

    feat [controller-EmployeeController] : Added EmplyeerController with endpoint for employee creation

---
+++ src/main/java/br/com/weswerikis/api_crud_employee/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package br.com.weswerikis.api_crud_employee.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.weswerikis.api_crud_employee.dto.EmployeeDto;
+import br.com.weswerikis.api_crud_employee.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
