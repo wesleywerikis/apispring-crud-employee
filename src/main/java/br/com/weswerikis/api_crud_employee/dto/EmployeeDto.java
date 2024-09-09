9b2ecb6a2a6e8b25e8cd133d1cd2613dd84f9c60
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-09 11:49:15 -0300

    feat [dto-EmployeeDto] : Added DTO EmployeeDto for data transfer

---
+++ src/main/java/br/com/weswerikis/api_crud_employee/dto/EmployeeDto.java
@@ -0,0 +1,20 @@
+package br.com.weswerikis.api_crud_employee.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+    
+    private Long id;
+    private String fisrtName;
+    private String lastName;
+    private String email;
+
+    
+}
