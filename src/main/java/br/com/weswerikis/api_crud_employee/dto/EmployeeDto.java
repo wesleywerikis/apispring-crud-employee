b8c19512481d0d59fa2fc8fa93599e42d88c1629
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-09 16:45:12 -0300

    fix [dto-EmployeeDto] : fixed variable name from fisrtName to firstName in EmployeeDto

--- src/main/java/br/com/weswerikis/api_crud_employee/dto/EmployeeDto.java
+++ src/main/java/br/com/weswerikis/api_crud_employee/dto/EmployeeDto.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 public class EmployeeDto {
     
     private Long id;
-    private String fisrtName;
+    private String firstName;
     private String lastName;
     private String email;
 
