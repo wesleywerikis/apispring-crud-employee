153e78992da0f1687c4d3fa4611ca2c2b2583366
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-10 13:59:58 -0300

    feat [service-impl-EmployeeServiceImpl] : Implemented getEmployeeById method in EmployeeServiceImpl with exception handling

--- src/main/java/br/com/weswerikis/api_crud_employee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/br/com/weswerikis/api_crud_employee/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.weswerikis.api_crud_employee.dto.EmployeeDto;
 import br.com.weswerikis.api_crud_employee.entity.Employee;
+import br.com.weswerikis.api_crud_employee.exception.ResourceNotFoundException;
 import br.com.weswerikis.api_crud_employee.mapper.EmployeeMapper;
 import br.com.weswerikis.api_crud_employee.repository.EmployeeRepository;
 import br.com.weswerikis.api_crud_employee.service.EmployeeService;
@@ -22,6 +23,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+        .orElseThrow(() -> new ResourceNotFoundException("Employee is not exist with given id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 
 }
