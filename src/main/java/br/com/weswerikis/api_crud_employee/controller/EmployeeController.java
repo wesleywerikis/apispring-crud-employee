8a414eb74d92139a3733b1033b85c6ca5381b6f4
Wesley Werikis <wesleywerikis@outlook.com>
2024-09-11 15:22:57 -0300

    feat [controller-EmployeeController] : Implemented DEL endpoint for deleting employees in EmployeeController

--- src/main/java/br/com/weswerikis/api_crud_employee/controller/EmployeeController.java
+++ src/main/java/br/com/weswerikis/api_crud_employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,11 @@ public class EmployeeController {
             @RequestBody EmployeeDto updateEmployee) {
         EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updateEmployee);
         return ResponseEntity.ok(employeeDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully!.");
     }
 }
